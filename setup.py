7be884c4f5c4de88be538d629dee0debe9c40b6a
Gaurav <sehgalgarv348@gmail.com>
2025-05-10 17:12:30 +0530

    Logging and exception added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Gaurav Sehgal",
+    author_email="sehgal_garv348@gmail.com",
+    packages=find_packages()
+)
